d303b36e78653c99f023d04e432534ee4a1a907b
salman <salmanfirdaus.m@gmail.com>
2024-03-27 23:41:11 +0700

    commit

---
+++ api/jenis/edit_jenis.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../../setup/koneksi.php');
+header('Content-Type: text/xml');
+$id = $_POST['id_jenis'];
+$jenis = $_POST['jenis'];
+$query = mysqli_query($koneksi,"UPDATE tbl_jenis SET nama_jenis = '$jenis' WHERE id_jenis = '$id'");
+if($query) {
+    $respons = [
+        'success' => 1,
+        'message' => "berhasil edit"
+    ];
+    echo json_encode($respons);
+} else {
+    $respons = [
+        'success' => 0,
+        'message' => "gagal edit"
+    ];
+    echo json_encode($respons);
+}
